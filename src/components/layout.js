890b0c0cc319527fd8448f5f022e7e0bbd245326
Giovanni Puntil <puntilgiovanni@gmail.com>
2020-05-06 09:54:42 +0100

    Removed social media links from footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -23,10 +23,6 @@ const Layout = ({ children, withHeader }) => {
         <footer>
           <div className="content">
             <div className="social-parent">
-              <div className="social">
-                <a href="https://www.linkedin.com/company/seenconnects" target="_blank" rel="noopener noreferrer" className="no-sub"><img alt="" src="/icons/linkedin.png" /></a>
-                <a href="https://www.instagram.com/connectscast/" target="_blank" rel="noopener noreferrer" className="no-sub"><img alt="" src="/icons/instagram.png" /></a>
-              </div>
               <div className="footer-logo-parent">
                 <img src="/icons/logo_with_type.png" alt="" className="footer-logo" />
                 <a href="https://seenconnects.com/" className="no-sub"><img alt="" className="footer-logo" src="/icons/connects-logo.png" /></a>
